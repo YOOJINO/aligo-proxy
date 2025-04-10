47bd508fa002ab1af77d169a1b498090f80a0f82
YOOJINO <lawofattraction2027@gmail.com>
2025-04-10 18:33:51 +0900

    Create aligo_server.py

---
+++ aligo_server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/send', methods=['POST'])
+def send_sms():
+    try:
+        aligo_url = "https://apis.aligo.in/send_mass/"
+        data = request.form.to_dict()
+        response = requests.post(aligo_url, data=data)
+        return jsonify(response.json())
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run()
